c0be0b84a90ec5996a05bb096fd95a0bb17a0ad0
O Jae O <47718603+OruteZ@users.noreply.github.com>
2024-06-22 00:05:13 +0900

    [Silver II] Title: 이진수 격자, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/31910. 이진수 격자/이진수 격자.cc
+++ 백준/Silver/31910. 이진수 격자/이진수 격자.cc
@@ -30,81 +30,56 @@ void input_map(vector<vector<int>>& map, int n) {
     }
 }
 
-void init(vector<vector<string>>& route, int n) {
+void init(vector<vector<LL>>& route, int n) {
     route.resize(n);
     for(int i = 0; i < n; i++) {
         route[i].resize(n);
-        for(string& value : route[i]) value = "";
+        for(LL& value : route[i]) value = 0;
     }
 }
 
-string str_max(string& a, string& b) {
-    if(a.empty()) return b;
-    if(b.empty()) return a;
+void tabulation(vector<vector<int>>& map, vector<vector<LL>>& route) {
+    int size = map.size();
+    auto get_value = [&size, &route](int i, int j)->LL {
+        if(i < 0 || i >= size) return -1;
+        if(j < 0 || j >= size) return -1;
 
-    if(a.size() != b.size()) {
-        return a.size() > b.size() ? a : b;
-    }
+        return route[i][j];
+    };
 
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) return a[i] > b[i] ? a : b;
-    }
+    for(int lv = 1; lv <= (size - 1) * 2; lv++) {
+        for(int i = 0; i <= lv; i++) {
+            int j = lv - i;
 
-    return a;
-}
+            if(i >= size) continue;
+            if(j >= size) continue;
+            if(i < 0) continue;
+            if(j < 0) continue;
 
-void bfs(vector<vector<int>>& map, vector<vector<string>>& route) {
-    pair<int, int> start = {0, 0};
-
-    queue<pair<int, int>> container;
-    container.push(start);
-
-    int di[2] = {0, 1};
-    int dj[2] = {1, 0};
-
-    while(not container.empty()) {
-        auto cur = container.front();
-        container.pop();
-
-        //next
-        for(int d = 0; d < 2; d++) {
-            pair<int, int> next = {cur.first + di[d], cur.second + dj[d]};
-            if(next.first >= map.size() || next.second >= map.size()) continue;
-
-            string& r = route[next.first][next.second];
-            int v = map[next.first][next.second];
-            string str = route[cur.first][cur.second] + to_string(v);
-
-            r = str_max(r, str);
-
-            container.push(next);
+            LL before_max = max(get_value(i - 1, j), get_value(i, j - 1));
+            route[i][j] = before_max * 2 + map[i][j];
         }
-    }
-}
 
-LL digit_to_int(string& str) {
-    LL v = 1;
-    LL ret = 0;
-    for(auto riter = str.rbegin(); riter != str.rend(); riter++) {
-        ret += (v) * (*riter == '1' ? 1 : 0);
-        v *= 2;
+//        for(int i = 0; i < size; i++) {
+//            for(int j = 0; j < size; j++) {
+//                cout << route[i][j] << ' ';
+//            } cout << ENDL;
+//        } cout << ENDL;
     }
-
-    return ret;
 }
 
 
 int main() {
     vector<vector<int>> map;
-    vector<vector<string>> route;
+    vector<vector<LL>> route;
 
     int n; cin >> n;
 
     input_map(map, n);
     init(route, n);
-    route[0][0] = to_string(map[0][0]);
+    route[0][0] = map[0][0];
 
-    bfs(map, route);
+    tabulation(map, route);
 
-    cout << digit_to_int(route.back().back());
+    cout << (route.back().back());
 }
