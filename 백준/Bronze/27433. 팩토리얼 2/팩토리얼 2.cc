48aa307b080e6996982711ee868c28bd281f3985
O Jae O <47718603+OruteZ@users.noreply.github.com>
2024-06-21 11:39:07 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.cc
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cmath>
+#include<queue>
+#include<algorithm>
+#include<stack>
+#include<array>
+#include<vector>
+#include<unordered_set>
+#include<list>
+
+#define LL long long
+#define PII std::pair<int, int>
+#define DEBUG true
+#define PRINT_LINE std::cout << "--------------------" << '\n'
+#define REPEAT(n) for(int _ = 0; _ < n; _++)
+#define FAST_IO std::ios_base::sync_with_stdio(false);std::cin.tie(nullptr);std::cout.tie(nullptr)
+#define ENDL '\n'
+#define INT_INF 2147483647
+#define INT_M_INF (-2147483648)
+
+int main() {
+    int n; std::cin >> n;
+
+    LL result = 1;
+    for(int i = 1; i <= n; i++) {
+        result *= i;
+    }
+
+    std::cout << result << ENDL;
+}
