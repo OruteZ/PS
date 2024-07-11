1d38a54c16076e76967c828ac879c26c9adc7e55
O Jae O <47718603+OruteZ@users.noreply.github.com>
2024-07-11 22:41:17 +0900

    [level 3] Title: 여행경로, Time: 0.05 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.cpp
+++ 프로그래머스/3/43164. 여행경로/여행경로.cpp
@@ -7,8 +7,13 @@
 
 using namespace std;
 
+// 공항 : <목적지, 티켓 번호>
 map<string, vector<pair<string, int>>> graph;
+
+// <n, b> : n번 티켓 썼는가? true : false (b)
 map<int, bool> visited;
+
+// 티켓 전체 개수
 int T_length;
 
 void build_graph(vector<vector<string>>& tickets) {
@@ -22,25 +27,27 @@ void build_graph(vector<vector<string>>& tickets) {
     }
 }
 
+// start 정점, edge_num 번 티켓을 써서, answer, stk
 bool try_dfs(string start, int edge_num, vector<string>& answer, int stk = 0) {
     if(visited[edge_num]) return false;
     
-    //cout << "DFS : " << start << ' ' << stk << endl;
+    cout << "DFS : " << start << ' ' << stk << endl;
     
     //start
     visited[edge_num] = true;
     answer.push_back(start);
     
+    
     //check fin
     if(T_length == stk) return true;
     
-    
-    for(auto& next : graph[start]) {
-        int next_edge = next.second;
-        string next_str = next.first;
-        bool result = try_dfs(next_str, next_edge, answer, stk + 1);
-        if(result == true) return true;
-    }
+        for(auto& next : graph[start]) {
+            int next_edge = next.second;
+            string next_str = next.first;
+            
+            bool result = try_dfs(next_str, next_edge, answer, stk + 1);
+            if(result == true) return true;
+        }
     
     
     //end
