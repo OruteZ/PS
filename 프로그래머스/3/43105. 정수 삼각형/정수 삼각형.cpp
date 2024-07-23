c088e59df0b1216c7817b040c70f7632ceb1d421
O Jae O <47718603+OruteZ@users.noreply.github.com>
2024-07-23 20:59:02 +0900

    [level 3] Title: 정수 삼각형, Time: 1.37 ms, Memory: 9.84 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+typedef long long ll;
+
+#define MAX 500
+ll cache[MAX][MAX] = {0,};
+
+void init() {
+    for(int i = 0; i < MAX; i++) {
+        for(int j = 0; j < i + 1; j++) {
+            cache[i][j] = -1;
+        }
+    }
+}
+
+int solution(vector<vector<int>> triangle) {
+    init();
+    
+    for(int i = triangle.size() - 1; i >= 0; i--) {
+        for(int j = 0; j < i + 1; j++) {
+            if(i == triangle.size() - 1) {
+                cache[i][j] = triangle[i][j];
+            }
+            
+            else {
+                cache[i][j] = triangle[i][j] + max(cache[i + 1][j], cache[i + 1][j + 1]);
+            }
+        }
+    }
+    
+    return cache[0][0];
+}
