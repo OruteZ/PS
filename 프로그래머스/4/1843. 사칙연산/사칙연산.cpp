c4c8d5c0983e2aa4adb33bff4c4e07f0196604c9
O Jae O <47718603+OruteZ@users.noreply.github.com>
2024-07-24 21:34:17 +0900

    [level 4] Title: 사칙연산, Time: 0.38 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/사칙연산.cpp
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.cpp
@@ -4,56 +4,64 @@
 #include <climits>
 using namespace std;
 
-int solution(vector<string> arr) {
-    int n = arr.size();
-    vector<int> nums;
-    vector<char> ops;
+vector<vector<int>> Max_table;
+vector<vector<int>> Min_table;
 
+vector<int> Numbers;
+vector<char> Operators;
+
+void init() {
+    int size = Numbers.size();
+
+    Max_table.assign(size + 1, vector<int>(size + 1, INT_MIN));
+    Min_table.assign(size + 1, vector<int>(size + 1, INT_MAX));
+
+    // DP 초기값 설정 (단일 숫자에 대한 경우)
+    for (int i = 0; i < size; i++) {
+        Max_table[i][i + 1] = Numbers[i];
+        Min_table[i][i + 1] = Numbers[i];
+
+        Max_table[i][i] = 0;
+        Min_table[i][i] = 0;
+    }
+}
+
+int solution(vector<string> arr) {
     // 숫자와 연산자 분리
-    for (int i = 0; i < n; i++) {
+    Numbers.clear(); Operators.clear();
+    for(int i = 0; i < arr.size(); i++) {
         if (i % 2 == 0) {
-            nums.push_back(stoi(arr[i]));
+            Numbers.push_back(stoi(arr[i]));
         } else {
-            ops.push_back(arr[i][0]);
+            Operators.push_back(arr[i][0]);
         }
     }
 
-    int l = nums.size();
-    // DP 테이블 초기화
-    vector<vector<int>> max_dp(l, vector<int>(l, INT_MIN));
-    vector<vector<int>> min_dp(l, vector<int>(l, INT_MAX));
-
-    // DP 초기값 설정 (단일 숫자에 대한 경우)
-    for (int i = 0; i < l; i++) {
-        max_dp[i][i] = nums[i];
-        min_dp[i][i] = nums[i];
-    }
+    init();
 
     // DP 점화식 채우기
-    for (int length = 2; length <= l; length++) {  // 부분 배열의 길이
-        for (int i = 0; i <= l - length; i++) {
-            int j = i + length - 1;
-            for (int k = i; k < j; k++) {
-                if (ops[k] == '+') {
-                    max_dp[i][j] = max(max_dp[i][j], max_dp[i][k] + max_dp[k+1][j]);
-                    min_dp[i][j] = min(min_dp[i][j], min_dp[i][k] + min_dp[k+1][j]);
-                } else if (ops[k] == '-') {
-                    max_dp[i][j] = max(max_dp[i][j], max_dp[i][k] - min_dp[k+1][j]);
-                    min_dp[i][j] = min(min_dp[i][j], min_dp[i][k] - max_dp[k+1][j]);
+    for (int length = 2; length <= Numbers.size(); length++) {  // 부분 배열의 길이
+
+        for (int begin = 0; begin <= Numbers.size() - length; begin++) {
+            int end = begin + length;
+
+            for (int mid = begin + 1; mid < end; mid++) {
+                char op = Operators[mid - 1];
+
+                if (op == '+') {
+                    Max_table[begin][end] =
+                            max(Max_table[begin][end], Max_table[begin][mid] + Max_table[mid][end]);
+                    Min_table[begin][end] =
+                            min(Min_table[begin][end], Min_table[begin][mid] + Min_table[mid][end]);
                 }
+                else { // -
+                    Max_table[begin][end] = max(Max_table[begin][end], Max_table[begin][mid] - Min_table[mid][end]);
+                    Min_table[begin][end] = min(Min_table[begin][end], Min_table[begin][mid] - Max_table[mid][end]);
+                }
+
             }
         }
     }
 
-    return max_dp[0][l-1];
-}
-
-int main() {
-    vector<string> arr1 = {"1", "-", "3", "+", "5", "-", "8"};
-    vector<string> arr2 = {"5", "-", "3", "+", "1", "+", "2", "-", "4"};
-
-    cout << solution(arr1) << endl;  // 결과: 1
-    cout << solution(arr2) << endl;  // 결과: 3
-
-    return 0;
+    return Max_table[0][Numbers.size()];
 }
