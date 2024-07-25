0d153b756508978a1bb7f063cc99f5e35fbd01d0
O Jae O <47718603+OruteZ@users.noreply.github.com>
2024-07-25 13:53:52 +0900

    [level 4] Title: 사칙연산, Time: 2.78 ms, Memory: 3.84 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/사칙연산.cpp
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.cpp
@@ -10,6 +10,45 @@ vector<vector<int>> Min_table;
 vector<int> Numbers;
 vector<char> Operators;
 
+int dp_max(int begin, int end);
+int dp_min(int begin, int end);
+
+int dp_max(int begin, int end) {
+    int& ret = Max_table[begin][end];
+    if(ret != INT_MIN) return ret;
+
+    for(int mid = begin + 1; mid < end; mid++) {
+        char op = Operators[mid - 1];
+
+        int left_max = dp_max(begin, mid);
+        int right_max = dp_max(mid, end);
+        int right_min = dp_min(mid, end);
+
+        if(op == '+') ret = max(ret, left_max + right_max);
+        else          ret = max(ret, left_max - right_min);
+    }
+
+    return ret;
+}
+
+int dp_min(int begin, int end) {
+    int& ret = Min_table[begin][end];
+    if(ret != INT_MAX) return ret;
+
+    for(int mid = begin + 1; mid < end; mid++) {
+        char op = Operators[mid - 1];
+
+        int left_min = dp_min(begin, mid);
+        int right_max = dp_max(mid, end);
+        int right_min = dp_min(mid, end);
+
+        if(op == '+') ret = min(ret, left_min + right_min);
+        else          ret = min(ret, left_min - right_max);
+    }
+
+    return ret;
+}
+
 void init() {
     int size = Numbers.size();
 
@@ -20,15 +59,13 @@ void init() {
     for (int i = 0; i < size; i++) {
         Max_table[i][i + 1] = Numbers[i];
         Min_table[i][i + 1] = Numbers[i];
-
-        Max_table[i][i] = 0;
-        Min_table[i][i] = 0;
     }
 }
 
 int solution(vector<string> arr) {
     // 숫자와 연산자 분리
-    Numbers.clear(); Operators.clear();
+    Numbers.clear(); 
+    Operators.clear();
     for(int i = 0; i < arr.size(); i++) {
         if (i % 2 == 0) {
             Numbers.push_back(stoi(arr[i]));
@@ -39,29 +76,5 @@ int solution(vector<string> arr) {
 
     init();
 
-    // DP 점화식 채우기
-    for (int length = 2; length <= Numbers.size(); length++) {  // 부분 배열의 길이
-
-        for (int begin = 0; begin <= Numbers.size() - length; begin++) {
-            int end = begin + length;
-
-            for (int mid = begin + 1; mid < end; mid++) {
-                char op = Operators[mid - 1];
-
-                if (op == '+') {
-                    Max_table[begin][end] =
-                            max(Max_table[begin][end], Max_table[begin][mid] + Max_table[mid][end]);
-                    Min_table[begin][end] =
-                            min(Min_table[begin][end], Min_table[begin][mid] + Min_table[mid][end]);
-                }
-                else { // -
-                    Max_table[begin][end] = max(Max_table[begin][end], Max_table[begin][mid] - Min_table[mid][end]);
-                    Min_table[begin][end] = min(Min_table[begin][end], Min_table[begin][mid] - Max_table[mid][end]);
-                }
-
-            }
-        }
-    }
-
-    return Max_table[0][Numbers.size()];
+    return dp_max(0, Numbers.size());
 }
