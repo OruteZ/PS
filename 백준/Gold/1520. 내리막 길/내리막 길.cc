c74006124682eff56a7aadc5219787aba1955848
O Jae O <47718603+OruteZ@users.noreply.github.com>
2024-09-12 23:14:30 +0900

    [Gold III] Title: 내리막 길, Time: 84 ms, Memory: 4524 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.cc
+++ 백준/Gold/1520. 내리막 길/내리막 길.cc
@@ -1,69 +1,58 @@
-#include <iostream>
-#include <cmath> 
-#pragma warning(disable:4996)
-#pragma warning(disable:6031)
+#include<iostream>
+#include<vector>
 using namespace std;
 
-#define MAX_NM 500
+#define MAX_SIZE (500)
 
-int n, m, arr[MAX_NM][MAX_NM], dp[MAX_NM][MAX_NM];
+int map[MAX_SIZE][MAX_SIZE] = {0, };
+int n, m;
+void input() {
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cin >> map[i][j];
+        }
+    }
+}
 
-const int dx[4] = { 0, 0, 1, -1 };
-const int dy[4] = { 1, -1, 0, 0 };
+int di[4] = {1, 0, -1, 0};
+int dj[4] = {0, 1, 0, -1};
 
-void check(int x0, int y0) {
-	int cnt = 0;
+int memo[MAX_SIZE][MAX_SIZE] = {0,};
+void memo_init() {
+    for(auto &i : memo) {
+        for(auto &j : i) {
+            j = -1;
+        }
+    }
+}
 
-	for (int d = 0; d < 4; d++) {
-		int x = x0 + dx[d];
-		int y = y0 + dy[d];
+int calculate_path(int i, int j) {
+    if(i == 0 and j == 0) return 1;
+    if(memo[i][j] != -1) return memo[i][j];
 
-		if (x < 0 || x >= n || y < 0 || y >= m) continue;
+    int result = 0;
 
-		if (arr[x][y] > arr[x0][y0]) cnt += dp[x][y];
-	}
+    int next_i, next_j;
+    for(int k = 0; k < 4; k++) {
+        next_i = di[k] + i;
+        next_j = dj[k] + j;
 
-	dp[x0][y0] = max(dp[x0][y0], cnt);
+        if(next_i >= n or next_i < 0) continue;
+        if(next_j >= m or next_j < 0) continue;
+        if(map[next_i][next_j] <= map[i][j]) continue;
+
+        result += calculate_path(next_i, next_j);
+    }
+
+    memo[i][j] = result;
+    return result;
 }
 
 int main() {
+    input();
+    memo_init();
+    cout << calculate_path(n - 1, m - 1) << endl;
 
-	scanf("%d %d", &n, &m);
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			scanf("%d", &arr[i][j]);
-		}
-	}
-
-	dp[0][0] = 1;
-
-	//최초 전파
-	for (int x0 = 0; x0 < n; x0++) {
-		for (int y0 = 0; y0 < m; y0++) {
-			check(x0, y0);
-		}
-	}
-
-	int max = n > m ? n : m;
-
-	for (int i = 0; i < (max - 1) / 2; i++) {
-
-		//역방향 전파
-		for (int x0 = n - 1; x0 >= 0; x0--) {
-			for (int y0 = m - 1; y0 >= 0; y0--) {
-				check(x0, y0);
-			}
-		}
-
-		//정방향 전파
-		for (int x0 = 0; x0 < n; x0++) {
-			for (int y0 = 0; y0 < m; y0++) {
-				check(x0, y0);
-			}
-		}
-	}
-
-	printf("%d", dp[n - 1][m - 1]);
-
-	return 0;
+    return 0;
 }
