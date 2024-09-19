4a8effbb1c97a0069056661f7a0160aeca84a4ea
O Jae O <47718603+OruteZ@users.noreply.github.com>
2024-09-19 23:20:51 +0900

    [Gold I] Title: 동전 뒤집기, Time: 68 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.cc
+++ 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.cc
@@ -5,124 +5,50 @@ using namespace std;
 
 // ================== 변수 정의 ==================
 #define MAX 20
-#define DEBUG false
-
 int N;
 bitset<MAX> Board[MAX];
-bitset<MAX> Backup[MAX];
 
 // answer 관련
-int ZeroCount = 987654321;
-
-// ================== 함수 정의 (After BackTracking) ==================
-
-void PrintBoard() {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            cout << Board[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << endl;
-}
-
-void FlipCol(int col) {
-    for (int i = 0; i < N; i++) {
-        Board[i].flip(col);
-    }
-}
-
-int CountZero(int col) {
-    int zeroCount = 0;
-    for (int i = 0; i < N; i++) {
-        zeroCount += Board[i][col] == 0;
-    }
-
-    return zeroCount;
-}
-
-// 각 행을 뒤집은 이후에, 열을 Greedy 하게 뒤집는다.
-// 이후 총 뒤집은 횟수와 뒷면의 개수를 계산해서 정답 갱신
-void FindAnswer() {
-    int zeroCount = 0;
-
-    for(int col = 0; col < N; col++) {
-        int cnt = CountZero(col);
-        if (cnt > N - cnt) {
-            FlipCol(col);
-        }
-
-        // fliped ? N - cnt : cnt
-        zeroCount += min(cnt, N - cnt);
-    }
-
-    if (zeroCount < ZeroCount) {
-        ZeroCount = zeroCount;
-    }
-
-    if (DEBUG) {
-        PrintBoard();
-    }
-}
-
-
-// ================== 함수 정의 (BackTracking) ==================
-
-void Recursion(stack<bool>& flipCase) {
-    if (flipCase.size() == N) {
-        // do flip
-        stack<bool> temp = flipCase;
-
-        for (int i = 0; i < N; i++) {
-            if (temp.top()) {
-                Board[i].flip();
-            }
-            temp.pop();
-        }
-        FindAnswer();
-
-        
-        // set board to backup
-        for (int i = 0; i < N; i++) {
-            Board[i] = Backup[i];
-        }
-
-        return;
-    }
-
-    flipCase.push(false);
-    Recursion(flipCase);
-    flipCase.pop();
-
-    flipCase.push(true);
-    Recursion(flipCase);
-    flipCase.pop();
-}
+int Answer = 987654321;
 
 void Init() {
-    cin >> N;
+	cin >> N;
 
-    char c;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            cin >> c;
+	char c;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> c;
 
-            Board[i][j] = c == 'H';
-            Backup[i][j] = Board[i][j];
-        }
-    }
+			Board[i][N - j - 1] = c == 'H';
+		}
+	}
 
-    
+
 }
 
 int main() {
-    Init();
+	Init();
 
-    stack<bool> flipCase;
+	bitset<MAX> mask;
+	for(int m = 0; m < (1 << N); m++) {
+		mask = m;
 
-    Recursion(flipCase);
+		int curResult = 0;
 
-    cout << ZeroCount << endl;
+		for(int i = 0; i < N; i++) {
+			//mask와 Board[i]를 XOR 연산
+			bitset<MAX> temp = mask ^ Board[i];
 
-    return 0;
+			int oneCount = temp.count();
+			int zeroCount = N - oneCount;
+
+			curResult += min(oneCount, zeroCount);
+		}
+
+		Answer = min(Answer, curResult);
+	}
+
+	cout << Answer << endl;
+
+	return 0;
 }
