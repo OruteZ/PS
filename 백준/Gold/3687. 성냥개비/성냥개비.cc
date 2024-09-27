32cefe6c87b2f7ef7a50766fdace303dab82d63f
O Jae O <47718603+OruteZ@users.noreply.github.com>
2024-09-27 18:18:45 +0900

    [Gold II] Title: 성냥개비, Time: 0 ms, Memory: 2340 KB -BaekjoonHub

---
+++ 백준/Gold/3687. 성냥개비/성냥개비.cc
@@ -0,0 +1,101 @@
+#include<bits/stdc++.h>
+
+
+#define LL long long int
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+using namespace std;
+
+int StickCost[10] = {6,2,5,5,4,5,6,3,7,6};
+vector<vector<LL>> Dp;
+
+
+void print_max(int n) {
+    // 정답은 항상 n / 2자릿수의 숫자
+    // n이 홀수일 경우, 7111... 형식
+    // n이 짝수일 경우, 1111... 형식
+    if(n % 2 == 0) {
+        for(int i = 0; i < n / 2; i++) {
+            cout << "1";
+        }
+    } else {
+        cout << "7";
+        for(int i = 0; i < n / 2 - 1; i++) {
+            cout << "1";
+        }
+    }
+}
+
+// len 길이의 최소값을 구하는 함수
+// 0을 리턴하면 불가능한 경우
+LL get_min_num(int len, int n, bool is_first = false) {
+    if(len == 1) {
+        for(int i = 0; i < 10; i++) {
+            if(is_first and i == 0) continue;
+            if(n - StickCost[i] == 0) {
+                return i;
+            }
+        }
+
+        return -100;
+    }
+
+    if(len == 2 and n == 8) {
+        return 10;  
+    }
+
+    LL& ret = Dp[len][n];
+    if(ret != -1) return ret;
+
+    ret = LLONG_MAX;
+    for(int i = 0; i < 10; i++) {
+        if(is_first and i == 0) continue;
+
+        if(n - StickCost[i] >= 0) {
+            LL next = get_min_num(len - 1, n - StickCost[i]);
+            if(next == -100) continue;
+
+            ret = min(ret, (LL)i * (LL)pow(10, len - 1) + next);
+        }
+    }
+
+    if(ret == LLONG_MAX) ret = -100;
+    return ret;
+}
+
+void print_min(int n) {
+    int length = 0;
+    if(n <= 7) length = 1;
+    else {
+        length = n / 7;
+        if(n % 7 != 0) length++;
+    }
+
+    Dp.assign(length + 1, vector<LL>(n + 1, -1));
+
+    LL ret = get_min_num(length, n, true);
+    cout << ret;
+}
+
+void solution() {
+    int n; 
+    cin >> n;
+
+    print_min(n);
+    cout << " ";
+    print_max(n);
+    cout << "\n";
+}
+
+int main() {
+    FASTIO;
+
+    int t_case;
+    cin >> t_case;
+
+    while(t_case--) {
+        solution();
+    }
+
+    return 0;
+}
