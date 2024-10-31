3c6d4f0c7068cec82065632af4b0db3b73735cc8
O Jae O <47718603+OruteZ@users.noreply.github.com>
2024-10-31 14:21:28 +0900

    [Platinum IV] Title: 경찰차, Time: 44 ms, Memory: 5984 KB -BaekjoonHub

--- 백준/Platinum/2618. 경찰차/경찰차.cc
+++ 백준/Platinum/2618. 경찰차/경찰차.cc
@@ -2,25 +2,25 @@
 
 using namespace std;
 
-typedef long long ll;
-
+// 모든 사건현장의 위치를 저장. 단 0번째와 1번쨰는 두 경찰차의 시작위치를 삽입
 vector<pair<int, int>> positions;
-vector<vector<ll>> dp;
+vector<vector<int>> dp;
 
-
+// positions[idx1]과 positions[idx2] 사이의 거리를 반환합니다.
 int dist(int idx1, int idx2) {
 	return abs(positions[idx1].first - positions[idx2].first) + abs(positions[idx1].second - positions[idx2].second);
 }
 
 // 각 경찰차들이 마지막으로 위치했던 곳
-ll findMinDistSum(int police1, int police2) {
+int findMinDistSum(int police1, int police2) {
 	int next = max(police1, police2) + 1;
 	if (next == positions.size()) return 0;
 
 	if (dp[police1][police2] != -1) return dp[police1][police2];
 
-	ll nextDist = dist(police1, next) + findMinDistSum(next, police2);
-	ll nextDist2 = dist(police2, next) + findMinDistSum(police1, next);
+	// 점화식 핵심 : 두 경찰차가 각각 다음 사건으로 이동했을때 결과값을 가져와서 최소값으로 변경
+	int nextDist = dist(police1, next) + findMinDistSum(next, police2);
+	int nextDist2 = dist(police2, next) + findMinDistSum(police1, next);
 
 	return dp[police1][police2] = min(nextDist, nextDist2);
 }
@@ -30,12 +30,15 @@ void trackLog(int police1, int police2) {
 	int next = max(police1, police2) + 1;
 	if (next == positions.size()) return;
 
-	ll nextDist = dist(police1, next) + findMinDistSum(next, police2);
-	ll nextDist2 = dist(police2, next) + findMinDistSum(police1, next);
+	int nextDist = dist(police1, next) + findMinDistSum(next, police2);
+	int nextDist2 = dist(police2, next) + findMinDistSum(police1, next);
 
+	// police1이 이동했을 때의 거리가 더 작다면 police1이 이동했다고 출력하고, police1이 이동한 경로를 출력합니다.
 	if (nextDist < nextDist2) {
 		cout << 1 << endl;
 		trackLog(next, police2);
+
+	// police2가 이동했을 때의 거리가 더 작다면 police2가 이동했다고 출력하고, police2가 이동한 경로를 출력합니다.
 	} else {
 		cout << 2 << endl;
 		trackLog(police1, next);
@@ -48,7 +51,7 @@ int main() {
 	int pos_size; cin >> pos_size;
 
 	positions.resize(pos_size + 2);
-	dp.resize(pos_size + 2, vector<ll>(pos_size + 2, -1));
+	dp.resize(pos_size + 2, vector<int>(pos_size + 2, -1));
 
 	positions[0] = {1, 1};
 	positions[1] = {n, n};
