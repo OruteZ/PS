0a30a5bf4505b3023bdbaf81b588d15084532305
O Jae O <47718603+OruteZ@users.noreply.github.com>
2025-04-27 14:35:09 +0900

    [Silver V] Title: 불사조, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/31780. 불사조/불사조.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long X;
+    int M;
+    cin >> X >> M;
+    cout << X * (M + 1) << "\n";
+    return 0;
+}
